929b5cc92206ba4763c1809b63dfee85cfba637e
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-11-07 21:50:49 +0700

    Add Mongoose Model

--- mongoose-connect.js
+++ mongoose-connect.js
@@ -1,10 +1,9 @@
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
+const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017");
+mongoose.connect('mongodb://localhost:27017');
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function () {
-  console.log("connected!");
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected!');
 });
