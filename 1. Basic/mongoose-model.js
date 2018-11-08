306c7275a85904b115fe115c0d2db937c1173101
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-11-09 01:20:49 +0700

    Rearrange Script

---
+++ 1. Basic/mongoose-model.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+//mongoose.connect("mongodb://localhost:27017/training");
+//support multi-connection
+var connection = mongoose.createConnection(
+  'mongodb://localhost:27017/training'
+);
+
+// 1. Create Schema
+var schema = new Schema({
+  name: 'string',
+  size: 'string'
+});
+
+// 2. Compile Schema
+var ModelTank = connection.model('ModelTank', schema);
+
+// 3. Create Document
+var DocumentBig = new ModelTank({
+  name: 'big',
+  size: 'big'
+});
+
+DocumentBig.save()
+  .then(function(doc) {
+    console.log(doc);
+    console.log('saved'); // saved!
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+// 4. Querying
+//we want to display all the tank we've seen.
+ModelTank.find(function(err, tanks) {
+  if (err) return console.error(err);
+  console.log(tanks);
+});
