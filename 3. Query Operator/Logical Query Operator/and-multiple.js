1789258cc7e50c49dacf58c7a0965b4940c4d702
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-11-09 13:27:28 +0700

    Add And Multiple Logic Operator

---
+++ 3. Query Operator/Logical Query Operator/and-multiple.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+//mongoose.connect("mongodb://localhost:27017/training");
+//support multi-connection
+var mongooseConnection = mongoose.createConnection(
+  'mongodb://localhost:27017/training'
+);
+
+// 1. Create Schema
+var schema = new Schema({
+  name: 'string',
+  age: {
+    type: Number,
+    min: [5, 'Too young'],
+    max: 99
+  },
+  wallet: { type: Number, required: [true, 'Add Me Money!'] }
+});
+
+// 2. Compile Schema
+var ModelMan = mongooseConnection.model('Man', schema);
+
+ModelMan.find({
+  $and: [
+    { $or: [{ wallet: 3000 }, { wallet: 5000 }] },
+    { $or: [{ age: 20 }, { age: { $lt: 30 } }] }
+  ]
+})
+  .then(function(doc) {
+    console.log(doc);
+    console.log(typeof doc);
+    console.log(doc[0]);
+    console.log(doc[0].name);
+    console.log('Document Fetched!');
+  })
+  .catch(err => console.log(err));
