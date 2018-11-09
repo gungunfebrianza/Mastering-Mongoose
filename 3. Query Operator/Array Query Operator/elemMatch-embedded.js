6500d072d7616be46ba48d5e9d363e2813040f05
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-11-10 04:08:36 +0700

    Add Array of Embedded Documents Example

---
+++ 3. Query Operator/Array Query Operator/elemMatch-embedded.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+//mongoose.connect("mongodb://localhost:27017/training");
+//support multi-connection
+var mongooseConnection = mongoose.createConnection(
+  'mongodb://localhost:27017/training'
+);
+
+// 1. Create Schema
+var ArraySchema = new Schema({
+  results: Schema.Types.Mixed
+});
+
+// 2. Compile Schema
+var ArrayModel = mongooseConnection.model('Array', ArraySchema);
+
+var arrDocuments = [
+  { results: [{ product: 'abc', score: 10 }, { product: 'xyz', score: 5 }] },
+  {
+    results: [{ product: 'abc', score: 8 }, { product: 'xyz', score: 7 }]
+  },
+  {
+    results: [{ product: 'abc', score: 7 }, { product: 'xyz', score: 8 }]
+  }
+];
+
+async function run() {
+  await ArrayModel.insertMany(arrDocuments)
+    .then(function(doc) {
+      console.log(doc);
+      console.log('success Insert!');
+    })
+    .catch(err => console.log(err));
+
+  await ArrayModel.find({
+    results: { $elemMatch: { product: 'xyz', score: { $gte: 8 } } }
+  })
+    .then(function(doc) {
+      console.log(doc);
+      console.log('Document Fetched!');
+    })
+    .catch(err => console.log(err));
+}
+
+run();
+
+/* 
+-----------------------------------------------
+This code has been Written By Gun Gun Febrianza
+Need Help ? Advice ? or Ask Question hit me at:
+gungunfebrianza@gmail.com
+
+Dont Hesitate to connect with me on social media:
+Facebook: www.facebook.com / papabitcoin
+Twitter: @daddybitcoin
+Instagram: mas.ggun */
