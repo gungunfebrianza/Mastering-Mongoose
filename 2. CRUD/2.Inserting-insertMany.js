4c66213bca2b1eba19c713228c1c45900747897f
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-11-09 16:27:47 +0700

    Update Inserting-insertMany Example

--- 2. CRUD/2.Inserting-insertMany.js
+++ 2. CRUD/2.Inserting-insertMany.js
@@ -8,40 +8,30 @@ var mongooseConnection = mongoose.createConnection(
 );
 
 // 1. Create Schema
-var schema = new Schema({
-  name: 'string',
-  age: {
-    type: Number,
-    min: [5, 'Too young'],
-    max: 99
-  },
-  wallet: { type: Number, required: [true, 'Add Me Money!'] }
+var ProductSchema = new Schema({
+  item: String,
+  qty: Number
 });
 
 // 2. Compile Schema
-var ModelMan = mongooseConnection.model('Man', schema);
+var ModelProduct = mongooseConnection.model('Product', ProductSchema);
 
 var arr = [
-  {
-    name: 'Gun Gun Febrianza',
-    age: 26,
-    wallet: 10000
-  },
-  {
-    name: 'Christa Agung Winarno',
-    age: 42,
-    wallet: 12500
-  }
+  { item: 'card', qty: 15 },
+  { item: 'envelope', qty: 20 },
+  { item: 'stamps', qty: 30 }
 ];
 
-ModelMan.insertMany(arr)
+ModelProduct.insertMany(arr)
   .then(function(doc) {
     console.log(doc);
     console.log('success Insert!');
   })
   .catch(err => console.log(err));
 
-/* This code has been Written By Gun Gun Febrianza
+/* 
+-------------------------------------------------
+This code has been Written By Gun Gun Febrianza
 Need Help ? Advice ? or Ask Question hit me at:
 gungunfebrianza@gmail.com
 
