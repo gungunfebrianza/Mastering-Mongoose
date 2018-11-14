9955d761416241531eb3e349d0e691d2896d8197
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-11-15 03:25:57 +0700

    Query Match with AND Operator

---
+++ 3. Querying In-Depth/Embedded Document/5.Query Match With And Operator.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+//mongoose.connect("mongodb://localhost:27017/training");
+//support multi-connection
+var mongooseConnection = mongoose.createConnection(
+  'mongodb://localhost:27017/training'
+);
+
+// 1. Create Schema
+var ArraySchema = new Schema({
+  item: String,
+  instock: Schema.Types.Mixed
+});
+
+// 2. Compile Schema
+var ArrayModel = mongooseConnection.model('EmbedDoc', ArraySchema);
+
+async function run() {
+  await ArrayModel.find({
+    'size.h': { $lt: 15 },
+    'size.uom': 'in',
+    status: 'D'
+  })
+    .then(function(doc) {
+      console.log('------ Result -------');
+      //console.log(doc);
+      for (let i = 0; i < doc.length; i++) {
+        console.log(doc[i]);
+      }
+      console.log('Document Fetched!');
+    })
+    .catch(err => console.log(err));
+}
+
+run();
+
+/* 
+-----------------------------------------------
+This code has been Written By Gun Gun Febrianza
+Need Help ? Advice ? or Ask Question hit me at:
+gungunfebrianza@gmail.com
+
+Dont Hesitate to connect with me on social media:
+Facebook: www.facebook.com / papabitcoin
+Twitter: @daddybitcoin
+Instagram: mas.ggun */
