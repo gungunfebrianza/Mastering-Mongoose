4fdce06e3ba43e40a2391a5a9977467d9656e752
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-11-15 13:38:45 +0700

    Query Specified Field Embedded

---
+++ 3. Querying In-Depth/1. Projection/5.Query-Specified-Field-Embedded.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+//mongoose.connect("mongodb://localhost:27017/training");
+//support multi-connection
+var mongooseConnection = mongoose.createConnection(
+  'mongodb://localhost:27017/training'
+);
+
+// 1. Create Schema
+var ArraySchema = new Schema({
+  item: String,
+  instock: Schema.Types.Mixed
+});
+
+// 2. Compile Schema
+var ArrayModel = mongooseConnection.model('Projection', ArraySchema);
+
+async function run() {
+  await ArrayModel.find({ status: 'A' }, { item: 1, status: 1, 'size.uom': 1 })
+    .then(function(doc) {
+      console.log('------ Result -------');
+      //console.log(doc);
+      for (let i = 0; i < doc.length; i++) {
+        console.log(doc[i]);
+      }
+      console.log('Document Fetched!');
+    })
+    .catch(err => console.log(err));
+
+  await ArrayModel.find({ status: 'A' }, { 'size.uom': 0 })
+    .then(function(doc) {
+      console.log('------ Result -------');
+      //console.log(doc);
+      for (let i = 0; i < doc.length; i++) {
+        console.log(doc[i]);
+      }
+      console.log('Document Fetched!');
+    })
+    .catch(err => console.log(err));
+}
+
+run();
+
+/* 
+-----------------------------------------------
+This code has been Written By Gun Gun Febrianza
+Need Help ? Advice ? or Ask Question hit me at:
+gungunfebrianza@gmail.com
+
+Dont Hesitate to connect with me on social media:
+Facebook: www.facebook.com / papabitcoin
+Twitter: @daddybitcoin
+Instagram: mas.ggun */
