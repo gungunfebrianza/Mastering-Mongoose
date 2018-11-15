13057e4744359e4671b5533e89b5d01c0363152f
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-11-15 14:06:21 +0700

    Existence Check

---
+++ 3. Querying In-Depth/5. Null & Missing Field/Existence-Check.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+//mongoose.connect("mongodb://localhost:27017/training");
+//support multi-connection
+var mongooseConnection = mongoose.createConnection(
+  'mongodb://localhost:27017/training'
+);
+
+// 1. Create Schema
+var ArraySchema = new Schema({
+  item: String,
+  instock: Schema.Types.Mixed
+});
+
+// 2. Compile Schema
+var ArrayModel = mongooseConnection.model('Array', ArraySchema);
+
+async function run() {
+  await ArrayModel.find({ item: { $exists: false } })
+    .then(function(doc) {
+      console.log(doc);
+      console.log('------ Result -------');
+      for (let i = 0; i < doc.length; i++) {
+        console.log(doc[i]);
+      }
+      console.log('Document Fetched!');
+    })
+    .catch(err => console.log(err));
+}
+
+run();
+
+/* 
+-----------------------------------------------
+This code has been Written By Gun Gun Febrianza
+Need Help ? Advice ? or Ask Question hit me at:
+gungunfebrianza@gmail.com
+
+Dont Hesitate to connect with me on social media:
+Facebook: www.facebook.com / papabitcoin
+Twitter: @daddybitcoin
+Instagram: mas.ggun */
