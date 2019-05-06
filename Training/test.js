97de31fd89bd3befcedb8500e6641e83e43da4f1
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-05-07 00:18:11 +0700

    Update Example

--- Training/test.js
+++ Training/test.js
@@ -35,6 +35,18 @@ var carnivoraDocuments = [
     live: ['Forest', 'Mountain']
   },
   {
+    type: 'Harimau',
+    name: 'Mod',
+    characteristic: {
+      weight: 250,
+      color: 'Yellow & Black',
+      food: ['Monkey', 'Sheep']
+    },
+    age: 24,
+    quantity: 2000,
+    live: ['River', 'Desert']
+  },
+  {
     type: 'Anjing',
     name: 'Krista',
     characteristic: {
@@ -45,17 +57,28 @@ var carnivoraDocuments = [
     age: 15,
     quantity: 59000,
     live: ['Forest', 'Home']
+  },
+  {
+    type: 'Anjing',
+    name: 'Matheo',
+    characteristic: {
+      weight: 30,
+      color: 'Red',
+      food: ['Fox', 'Fish']
+    },
+    age: 10,
+    quantity: 12000,
+    live: ['Forest', 'Home']
   }
 ];
 
 async function run_insertMany() {
   await MamaliaModel.insertMany(carnivoraDocuments)
-    .then(function(doc) {
-      console.log(doc);
+    .then(function(documents) {
       console.log('Insert Success!');
       console.log('------ Result -------');
-      for (let i = 0; i < doc.length; i++) {
-        console.log(doc[i]);
+      for (let i = 0; i < documents.length; i++) {
+        console.log(documents[i]);
       }
     })
     .catch(err => console.log(err));
